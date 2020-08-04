677de2a4150d99831ccb454582e5114bf0c8b7ae
Sebastian Tomczak <sebas.tomczak@gmail.com>
2020-08-04 22:37:24 +0200

    Startup - Grid. Base Tilemaps with Palettes and Titlesets. Some first Sprites. Catalogs in project. Movement scripts with/for Player. Basic stuff. Some changes in Physic2D + Quality.

---
+++ Miner/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    [SerializeField]
+    private float jumpForce = 2f;
+
+    private Rigidbody2D _rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMovement();
+        Jump();
+        FlipSprite();
+    }
+
+    private void HorizontalMovement()
+    {
+        var movementInput = Input.GetAxis("Horizontal");
+        var movementValue = movementInput * movementSpeed * Time.deltaTime * 100;
+
+        Vector2 playerVelocity = new Vector2(movementValue, _rigidBody2D.velocity.y);
+        _rigidBody2D.velocity = playerVelocity;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rigidBody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FlipSprite()
+    {
+        var playerHasHorizontalSpeed = Mathf.Abs(_rigidBody2D.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(_rigidBody2D.velocity.x), 1f);
+        }
+    }
+}
